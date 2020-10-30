ff6c97eeaff153591843d71f77ee858e1b801540
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2020-10-30 17:50:58 +0100

    fixed some installer bugs

--- installer/create_installer.bat
+++ installer/create_installer.bat
@@ -2,8 +2,9 @@ REM harvest all files from share/OpenMS folder and automatically generate compon
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\heat" dir "SourceDir\share\OpenMS" -gg -sfrag -sreg -template component -cg OpenMSShareComponents -dr PDSystemReleaseNuXLShareOpenMSFiles -o "OpenMSShareComponents.wxs"
 REM compile to wixobj
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle" "OpenMSShareComponents.wxs" "NuXLMain.wxs" -arch x64
-REM link into msi installer
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\light" -o PDNuXLNodes.msi NuXLMain.wixobj OpenMSShareComponents.wixobj -b "SourceDir\share\OpenMS"
-REM create executable installer from wsi file
+REM link into msi installer (silence warning about same version upgrade ICE61)
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light" -sice:ICE61 -o PDNuXLNodes.msi NuXLMain.wixobj OpenMSShareComponents.wixobj -b "SourceDir\share\OpenMS"
+REM create executable installer from wsi file (silence warning about same version upgrade ICE61)
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle" -ext WixNetFxExtension -ext WixBalExtension -ext WixUtilExtension ExecutableInstaller.wxs -arch x64
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -ext WixNetFxExtension -ext WixBalExtension -ext WixUtilExtension ExecutableInstaller.wixobj
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -sice:ICE61 -ext WixNetFxExtension -ext WixBalExtension -ext WixUtilExtension ExecutableInstaller.wixobj
+REM debug with msiexec /i MyApplication.msi /l*v MyLogFile.txt
