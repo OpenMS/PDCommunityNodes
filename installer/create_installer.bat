0b57dea791e35e2903c2e3360389982f1167fd0b
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2020-11-05 15:58:14 +0100

    fix error reported by Kai: share in wrong folder

--- installer/create_installer.bat
+++ installer/create_installer.bat
@@ -1,5 +1,5 @@
 REM harvest all files from share/OpenMS folder and automatically generate components, GUIDs so we don't need to do this manually for hundreds of files.
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat" dir "SourceDir\share\OpenMS" -gg -sfrag -sreg -template component -cg OpenMSShareComponents -dr PDSystemReleaseNuXLShareOpenMSFiles -o "OpenMSShareComponents.wxs"
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat" dir "SourceDir\share\OpenMS" -gg -sfrag -sreg -template component -cg OpenMSShareComponents -dr PDSystemReleaseNuXLShareFiles -o "OpenMSShareComponents.wxs"
 REM compile to wixobj
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle" "OpenMSShareComponents.wxs" "NuXLMain.wxs" -arch x64
 REM link into msi installer (silence warning about same version upgrade ICE61)
